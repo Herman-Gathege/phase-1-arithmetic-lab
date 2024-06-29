50e7e4fa0b89c51f465ca2158c0ac9f9712d49dd
Herman-Gathege <siinamtechstudio@gmail.com>
2024-06-29 13:38:41 +0300

    change math

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
 
 // Write your code here
+// Using Math.random() method
+
+let multiply = "62";
+function multiply() {
+  return num1*num2;
+  }
+multiply();
+
+function random () {
+    Number.isInteger (random !=0);
+    return (isInteger);
+    }
+random();
+
+ let remainder = "4"   
+function mod () {
+    return (num3, num4);
+}    
+mod();
+
+function max() {
+  return math.max (0, 20);
+}
+max();
